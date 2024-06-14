f93cb21747f9d2258a71796965dd798b2799b612
ankitaingle <aningle31@gmail.com>
2024-06-14 14:04:46 -0400

    Add validation constraints on Request Body

--- runnerz/src/main/java/dev/ankitaingle/runnerz/run/RunController.java
+++ runnerz/src/main/java/dev/ankitaingle/runnerz/run/RunController.java
@@ -1,10 +1,9 @@
 package dev.ankitaingle.runnerz.run;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,13 +36,13 @@ public class RunController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    void create(@RequestBody Run run) {
+    void create(@Valid @RequestBody Run run) {
         runRepository.create(run);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id) {
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
         runRepository.update(run, id);
     }
 
