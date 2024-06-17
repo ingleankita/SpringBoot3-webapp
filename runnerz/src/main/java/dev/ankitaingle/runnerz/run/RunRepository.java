fcff3f9bcffe300b450678d435de8d7f160157c4
ankitaingle <aningle31@gmail.com>
2024-06-17 08:47:28 -0400

    Comment code for in-memory list data structure to store runs

--- runnerz/src/main/java/dev/ankitaingle/runnerz/run/RunRepository.java
+++ runnerz/src/main/java/dev/ankitaingle/runnerz/run/RunRepository.java
@@ -1,6 +1,10 @@
 package dev.ankitaingle.runnerz.run;
 
+import dev.ankitaingle.runnerz.Application;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -12,6 +16,20 @@ import java.util.Optional;
 /* The @Repository annotation is used to indicate that a class is a Data Access Object (DAO), which will handle database operations */
 @Repository
 public class RunRepository {
+
+    private static final Logger log = LoggerFactory.getLogger(RunRepository.class);
+    private final JdbcClient jdbcClient;
+
+    public RunRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    public List<Run> findAll() {
+        return jdbcClient.sql("SELECT * FROM Run").query(Run.class).list();
+    }
+
+/*  Initial in-memory list data structure to store runs
+
     private List<Run> runs = new ArrayList<>();
 
     // Get all runs
@@ -43,10 +61,11 @@ public class RunRepository {
     /*
      The @PostConstruct annotation is used to mark a method that should be executed after the dependency injection is
      done to perform any initialization. This method runs only once after the bean's properties have been set.
-    */
+    * /
     @PostConstruct
     private void init() {
         runs.add(new Run(1, "Monday Morning Run", LocalDateTime.now(), LocalDateTime.now().plus(30, ChronoUnit.MINUTES), 3, Location.INDOOR));
         runs.add(new Run(2, "Wednesday Evening Run", LocalDateTime.now(), LocalDateTime.now().plus(60, ChronoUnit.MINUTES), 6, Location.INDOOR));
     }
+*/
 }
